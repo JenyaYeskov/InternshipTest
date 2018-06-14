088e107d9a24ca0b0a93d758654823516be3749a
Zhenya <jeskov95@gmail.com>
2018-06-15 00:38:40 +0300

    Did University

--- src/institution/University.java
+++ src/institution/University.java
@@ -1,17 +1,54 @@
 package institution;
 
 import person.Student;
+import person.consciousness.Knowledge;
+
+import java.util.*;
 
 public class University {
+    private final String name;
+    private Map<Integer, Student> universityStudents = new HashMap<>();
+
     public University(String name) {
-        //TODO: Implementation is needed
+        this.name = name;
     }
 
-    public void setStudent(Student student) {
-        //TODO: Implementation is needed
+    public Map<Integer, Student> getStudents() {
+        return universityStudents;
+    }
+
+    public void setStudent(String name, String newName, int knowledge) {
+        for (Map.Entry<Integer, Student> st : universityStudents.entrySet()) {
+            if (Objects.equals(st.getValue().getName(), name)) {
+                universityStudents.get(st.getKey()).setName(newName);
+                universityStudents.get(st.getKey()).setKnowledge(new Knowledge(knowledge));
+            }
+        }
     }
 
     public void addStudent(Student student) {
-        //TODO: Implementation is needed
+        if (!universityStudents.containsValue(student)) {
+            int id = universityStudents.size();
+            while (universityStudents.containsKey(id)) {
+                id++;
+            }
+            universityStudents.put(id, student);
+        }
     }
+
+    public void addStudent(Student student, int id) {
+        if (!universityStudents.containsValue(student))
+            universityStudents.put(id, student);
+    }
+
+    public double getAverage() {
+        List<Integer> vals = new ArrayList<>();
+
+        for (Student s : universityStudents.values()) {
+            vals.add(s.getKnowledge().getLevel());
+        }
+
+        return Arrays.stream(vals.stream().mapToInt(i -> i).toArray()).average().orElse(Double.NaN);
+    }
+
 }
